8d1a60309f2298273808bec3e519565a6ac35f80
NotAShelf <raf@notashelf.dev>
2025-10-25 16:24:23 +0300

    nixos/libvirtd: fix shellcheck warning
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I823e6487a30782e8592854e1c08a73d36a6a6964

--- nixos/modules/virtualisation/libvirtd.nix
+++ nixos/modules/virtualisation/libvirtd.nix
@@ -480,11 +480,12 @@ in
         ln -s --force ${cfg.qemu.package}/bin/qemu-pr-helper /run/${dirName}/nix-helpers/
 
         # Symlink to OVMF firmware code and variable template images distributed with QEMU
-        cp -sfv $(
-          ${pkgs.jq}/bin/jq -rs \
-            '[.[] | .mapping.executable.filename, .mapping."nvram-template".filename] | unique | .[]' \
-          ${cfg.qemu.package}/share/qemu/firmware/* \
-        ) /run/${dirName}/nix-ovmf
+
+        for file in $(${pkgs.jq}/bin/jq -rs \
+            '.[] | .mapping.executable.filename, .mapping."nvram-template".filename] | unique | .[]' \
+            ${cfg.qemu.package}/share/qemu/firmware/*); do
+          cp -sfv -- "$file" "/run/${dirName}/nix-ovmf"
+        done
 
         # Symlink hooks to /var/lib/libvirt
         ${concatStringsSep "\n" (
