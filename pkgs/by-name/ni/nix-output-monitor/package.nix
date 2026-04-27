314397b6d9e69a8daca7392c2b17e8e6b33ee601
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2026-04-27 19:22:27 -0400

    nix-output-monitor: use custom symbols

--- pkgs/by-name/ni/nix-output-monitor/package.nix
+++ pkgs/by-name/ni/nix-output-monitor/package.nix
@@ -6,6 +6,8 @@
   extraComposeFunctions ? [ ],
 }:
 let
+  inherit (lib.lists) zipListsWith;
+  inherit (lib.strings) concatLines escapeShellArg;
   inherit (haskell.lib.compose) justStaticExecutables overrideCabal;
 
   overrides = {
@@ -19,6 +21,40 @@ let
     ];
 
     buildTools = [ installShellFiles ];
+
+    postPatch =
+      let
+        oldIcons = [
+          "↑"
+          "↓"
+          "⏱"
+          "⏵"
+          "✔"
+          "⏸"
+          "⚠"
+          "∅"
+          "∑"
+        ];
+        newIcons = [
+          "f062" # 
+          "f063" # 
+          "f520" # 
+          "f04b" # 
+          "f00c" # 
+          "f04c" # 
+          "f071" # 
+          "f1da" # 
+          "f04a0" # 󰒠
+        ];
+      in
+      assert __length oldIcons == __length newIcons;
+      ''
+        sed -i ${
+          escapeShellArg (concatLines (zipListsWith (o: n: "s/${o}/\\\\x${n}/") oldIcons newIcons))
+        } lib/NOM/Print.hs
+        sed -i 's/┌/╭/' lib/NOM/Print/Tree.hs
+      '';
+
     postInstall = ''
       ln -s nom "$out/bin/nom-build"
       ln -s nom "$out/bin/nom-shell"
